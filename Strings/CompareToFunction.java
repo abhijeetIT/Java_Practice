45bd24e806d52a4bfb99920f26b463fe8a767366
abhijeetIT <173333375+abhijeetIT@users.noreply.github.com>
2026-09-17 12:03:27 +0530

    compareTo learned

---
+++ Strings/CompareToFunction.java
@@ -0,0 +1,16 @@
+package main.java.JavaBasic.Strings;
+
+public class CompareToFunction {
+    public static void main(String[] args) {
+        
+        String str1 = "ABHI";
+        String str2 = "XYZ";
+        String str = "ABHI";
+
+        System.out.println(str1.compareTo(str2));
+        System.out.println(str2.compareTo(str1));
+        System.out.println(str1.compareTo(str));
+        
+
+    }
+}
