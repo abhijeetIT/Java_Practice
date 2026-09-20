f765146066b57b0e5b22358ff14e966364179077
abhijeetIT <173333375+abhijeetIT@users.noreply.github.com>
2026-09-20 19:49:43 +0530

    HashMap quetion2 practice

---
+++ HashMap_dict/Quetion2.java
@@ -0,0 +1,13 @@
+package main.java.JavaBasic.HashMap_dict;
+
+import java.util.HashMap;
+
+public class Quetion2 {
+    public static void main(String[] args) {
+        HashMap<Integer,String> map = new HashMap<>();
+
+        map.put(1, "Hii");
+
+        System.out.println(map.get(2));
+    }
+}
