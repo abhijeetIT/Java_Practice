d5b375968079db948ed28d85959c245e75dac957
abhijeetIT <173333375+abhijeetIT@users.noreply.github.com>
2026-09-22 20:03:40 +0530

    Synchronized practice

---
+++ Threding/SyncronizePractice.java
@@ -0,0 +1,60 @@
+class Counter {
+    int count = 0;
+
+    /* void increment() {
+        count++;
+    }
+*/
+
+//this one is lock the fucntion while one using 
+synchronized void increment() {  
+        count++;
+    }
+
+}
+
+public class SyncronizePractice {
+    public static void main(String[] args) throws InterruptedException {
+
+        Counter counter = new Counter();
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                counter.increment();
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                counter.increment();
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println(counter.count);
+    }
+}
+
+
+
+/*Thread 1 reads count
+
+count = 0
+
+Thread 2 reads count
+
+count = 0 (before Thread 1 writes)
+
+Thread 1 writes
+
+count = 1
+
+
+Thread 2 writes
+
+count = 1 (overwrites Thread 1's update) */
