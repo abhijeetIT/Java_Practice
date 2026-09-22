3cf163736a5ae54892ba98aa0a2ee0f5529ec5df
abhijeetIT <173333375+abhijeetIT@users.noreply.github.com>
2026-09-22 18:33:06 +0530

    Understanding the throw

--- ExceptionHandling/FinallyBlock.java
+++ ExceptionHandling/FinallyBlock.java
@@ -6,14 +6,15 @@ public class FinallyBlock {
     public static int practice(){
         
         try{
+            System.out.println("IM try");
             return 10;
         }catch(Exception e){
             return 20;
         }finally{
+            System.out.println("Im the boss");
             return 30;
         }
     }
-
     public static void main(String[] args) {
         
         System.out.println(practice());
