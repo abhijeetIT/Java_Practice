fcbfdd5c7fbf00f3c2ad32ba707cfb5f8fb2bf8f
abhijeetIT <173333375+abhijeetIT@users.noreply.github.com>
2026-09-23 10:43:32 +0530

    New loop practice

---
+++ Loops/ExploringLoop2.java
@@ -0,0 +1,12 @@
+package main.java.JavaBasic.Loops;
+
+public class ExploringLoop2 {
+    public static void main(String[] args) {
+        
+
+        //this one also gives infinite interation
+        for(int i=0; ;i++){
+           System.out.println(i);
+        }
+    }
+}
