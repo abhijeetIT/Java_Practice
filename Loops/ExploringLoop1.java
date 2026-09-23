ed129845bcd36187bedadb1e9afab4a8798c6ea6
abhijeetIT <173333375+abhijeetIT@users.noreply.github.com>
2026-09-23 10:40:50 +0530

    Understanding different situation of loops

---
+++ Loops/ExploringLoop1.java
@@ -0,0 +1,13 @@
+package main.java.JavaBasic.Loops;
+
+public class ExploringLoop1 {
+    public static void main(String[] args) {
+        int i=0;
+
+        //this loops gives infinite interation
+        for( ; ; ){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
