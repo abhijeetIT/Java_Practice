ec0e2664a1806ee541afe4c907cc4ec6701e0488
abhijeetIT <173333375+abhijeetIT@users.noreply.github.com>
2026-09-23 10:45:53 +0530

    Withoout 3rd condition

---
+++ Loops/ExploringLoop3.java
@@ -0,0 +1,12 @@
+package main.java.JavaBasic.Loops;
+
+public class ExploringLoop3 {
+    
+    public static void main(String[] args) {
+        
+        //this one also give infinite loop becouse it not increment the i
+        for(int i=0 ; i <100; ){
+            System.out.println(i);
+        }
+    }
+}
