0f2dcb818525b5c8afe523c3dcd0d2d830f91acc
abhijeetIT <173333375+abhijeetIT@users.noreply.github.com>
2026-09-26 16:48:39 +0530

    New Practice interview

--- Array/NumberOfVowels.java
+++ Array/NumberOfVowels.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class NumberOfVowels {
-    
+
+    public static int countVowels(String str){
+        int count=0;
+
+        List<Character> vowelsList = List.of('a','e','i','o','u','A','E','I','O','U');
+
+        for(int i=0;i<str.length();i++){
+            if(vowelsList.contains(str.charAt(i))){
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Total vowels in the list: "+countVowels("Abhijeet"));
+        
+    }
 }
